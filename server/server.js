32823e7ab867cf4959af68bdb848776457a28ca0
jpetliuk <jeremias.petliuk@gmail.com>
2025-02-11 23:11:00 -0300

    added helmet as a dependency

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import helmet from 'helmet';
 
 import { db } from './config/db.js';
 import userRoutes from './routes/userRoutes.js';
@@ -10,6 +11,7 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(cors());
+app.use(helmet());
 
 const PORT = process.env.PORT || 5000;
 
