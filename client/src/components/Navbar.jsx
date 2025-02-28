1dc19c91f40c63d3f42b80969e199229547823ac
jpetliuk <jeremias.petliuk@gmail.com>
2025-02-28 03:41:42 -0300

    feat: enhance Navbar component with project integration and new styling for buttons

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -2,9 +2,13 @@ import { Link } from 'react-router-dom';
 import { useState, useEffect } from 'react';
 import { useLocation, useParams } from 'react-router-dom';
 
-import { tools, projects } from '../utils/MockData';
+import { tools } from '../utils/MockData';
+
+import useUserStore from '../store/userStore';
 
 const Navbar = () => {
+  const { projects } = useUserStore();
+
   const location = useLocation();
   const { id } = useParams();
 
@@ -39,7 +43,7 @@ const Navbar = () => {
           {tools.map((item) => (
             <Link to={item.path} key={item.label}>
               <div
-                className={`hover:bg-white-gray flex w-full cursor-pointer items-center gap-2 rounded-3xl py-1.5 pl-4 duration-200 ${selected === item.path ? 'bg-white-gray' : 'bg-none'}`}
+                className={`hover:bg-navbar-select-button flex w-full cursor-pointer items-center gap-2 rounded-3xl py-1.5 pl-4 duration-200 ${selected === item.path ? 'bg-navbar-select-button' : 'bg-none'}`}
               >
                 <img
                   src={item.icon}
@@ -58,27 +62,34 @@ const Navbar = () => {
         {/* Tools builder */}
 
         {/* Projects builder */}
-        <div className="border-white-gray mt-2 flex w-full flex-col gap-1 border-b pb-4">
+        <div className="border-white-gray mt-2 flex w-full flex-col gap-1 border-b pb-1.5">
           <h2 className="text-default-text text-base font-semibold">
             Projects
           </h2>
 
-          {projects.map((item) => (
-            <Link key={item._id} to={`/dashboard/project/${item._id}`}>
+          {projects.map((project) => (
+            <Link key={project._id} to={`/dashboard/project/${project._id}`}>
               <div
-                className={`hover:bg-white-gray flex w-full cursor-pointer items-center gap-2 rounded-3xl py-1.5 pl-4 duration-200 ${selected === item._id ? 'bg-white-gray' : 'bg-none'}`}
+                className={`hover:bg-navbar-select-button flex w-full cursor-pointer items-center gap-2 rounded-3xl py-1.5 pl-4 duration-200 ${selected === project._id ? 'bg-navbar-select-button' : 'bg-none'}`}
               >
-                <p className="font-bold" style={{ color: item.iconColor }}>
+                <p className="font-bold" style={{ color: project.iconColor }}>
                   #
                 </p>
                 <h3
-                  className={`${selected === item._id ? 'text-orange-text font-semibold' : 'text-light-text font-light'} text-[15px] duration-100`}
+                  className={`${selected === project._id ? 'text-orange-text font-semibold' : 'text-light-text font-light'} text-[15px] duration-100`}
                 >
-                  {item.projectName}
+                  {project.projectName}
                 </h3>
               </div>
             </Link>
           ))}
+          <div className="hover:text-default-text h-4 overflow-hidden text-transparent duration-200 hover:h-9">
+            <div className="hover:bg-navbar-create-button flex w-full cursor-pointer items-center justify-center gap-2 rounded-3xl py-1.5 duration-200">
+              <h3 className="mr-5 text-center text-[15px] font-light select-none">
+                Create new Project
+              </h3>
+            </div>
+          </div>
         </div>
         {/* Projects builder */}
       </div>
