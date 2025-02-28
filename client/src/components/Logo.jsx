b3904f3be7c82219445fe4382d185143561af2c5
jpetliuk <jeremias.petliuk@gmail.com>
2025-02-28 02:30:20 -0300

    add new logo icon and update Logo component to use the new image

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,11 +1,7 @@
 const Logo = () => {
   return (
-    <div className="border-outline bg-custom-white flex h-14 w-full items-center justify-center gap-2 rounded-3xl border">
-      <img
-        src="/icon.png"
-        alt="icon description"
-        className="w-9 rounded-full p-0"
-      />
+    <div className="border-outline bg-custom-white flex h-14 w-full items-center gap-2 rounded-3xl border">
+      <img src="/icon2.png" alt="icon" className="ml-3 h-9 w-9 rounded-full" />
       <h1 className="text-default-text my-auto text-center text-lg font-bold">
         HANNAH&apos;S LIST LAB
       </h1>
