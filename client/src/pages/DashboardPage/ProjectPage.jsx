63ff92e5de7f35feaa7b7aeb7e24b078a2506985
jpetliuk <jeremias.petliuk@gmail.com>
2025-03-02 03:18:06 -0300

    feat: create ProjectDisplayer component and integrate it into ProjectPage

--- client/src/pages/DashboardPage/ProjectPage.jsx
+++ client/src/pages/DashboardPage/ProjectPage.jsx
@@ -2,9 +2,8 @@ import { useParams } from 'react-router-dom';
 import useUserStore from '../../store/userStore';
 
 import NotFound404Page from './NotFound404Page';
+import ProjectDisplayer from '../../components/ProjectPage/ProjectDisplayer';
 
-import ItemTaskPrinter from '../../components/ProjectPage/ItemTaskPrinter';
-import { SlidersHorizontal, Plus } from 'lucide-react';
 import { useState } from 'react';
 import { useEffect } from 'react';
 
@@ -23,46 +22,7 @@ const ProjectPage = () => {
 
   return (
     <div className="border-outline bg-custom-white min-h-full w-full rounded-3xl border">
-      {currentProject ? (
-        <div className="min-h-full w-full p-4">
-          <div
-            style={{
-              backgroundImage: `url(${currentProject.backgroundImage})`,
-            }}
-            className="bg-light-gray relative mb-10 flex h-64 w-full justify-center rounded-3xl bg-cover bg-center"
-          >
-            <h1 className="text-custom-white absolute bottom-14 mx-2 max-w-[500px] px-4 text-center text-4xl font-bold underline decoration-2 underline-offset-5">
-              {currentProject.projectName}
-            </h1>
-            <div className="bg-light-transparent-gray hover:bg-light-transparent-gray-hover active:bg-light-transparent-gray text-light-text hover:text-default-text absolute top-0 left-0 flex h-10 w-40 cursor-pointer items-center justify-center gap-1.5 rounded-tl-3xl rounded-br-3xl">
-              <SlidersHorizontal className="h-4 w-4" />
-              <h3 className="text-base font-bold">Settings</h3>
-            </div>
-          </div>
-
-          {currentProject.description ? (
-            <div className="border-comment-blue-line bg-comment-blue-background m-auto w-10/12 border-l-4">
-              <p className="p-5 text-left whitespace-pre-line">
-                {currentProject.description}
-              </p>
-            </div>
-          ) : null}
-
-          <div className="mt-16">
-            <div className="relative mb-3">
-              <input
-                autoComplete="off"
-                className="text-light-text border-light-gray w-full rounded-2xl border py-2.5 pl-20 focus:outline-offset-2"
-                placeholder="Add New Task"
-              />
-              <Plus className="text-light-text border-light-gray hover:text-default-text absolute top-0 h-full w-12 cursor-pointer rounded-l-2xl border border-r bg-[#FBB19D] p-2" />
-            </div>
-            <ItemTaskPrinter items={currentProject.items} />
-          </div>
-        </div>
-      ) : (
-        <NotFound404Page />
-      )}
+      {currentProject ? <ProjectDisplayer currentProject={currentProject} /> : <NotFound404Page />}
     </div>
   );
 };
