91838285a85049d4803299de2ccc3f98532010e7
jpetliuk <jeremias.petliuk@gmail.com>
2025-03-01 23:57:25 -0300

    feat: update JumpingNote component styling for improved visual appearance

--- client/src/components/Loaders/JumpingNote.jsx
+++ client/src/components/Loaders/JumpingNote.jsx
@@ -2,7 +2,7 @@ import './JumpingNote.css';
 
 const JumpingNote = () => {
   return (
-    <div className="bg-white-gray flex h-screen items-center justify-center">
+    <div className="border-outline bg-custom-white flex h-full w-full items-center justify-center rounded-3xl border">
       <div className="scale-150">
         <div className="relative h-12 w-12">
           <div className="loader-shadow"></div>
