ac489690dc2993d33b1281b07bd546bdede4890d
jpetliuk <jeremias.petliuk@gmail.com>
2025-03-04 09:34:25 -0300

    feat: remove unused image files and update logo image source; wrap app in StrictMode for improved performance

--- client/src/components/Logo.jsx
+++ client/src/components/Logo.jsx
@@ -1,7 +1,7 @@
 const Logo = () => {
   return (
     <div className="border-outline bg-custom-white flex h-14 w-full items-center gap-2 rounded-3xl border">
-      <img src="/icon-hannahsListLab.png" alt="icon" className="ml-3 h-9 w-9" />
+      <img src="/hannahListLabIcon.png" alt="icon" className="ml-3 h-9 w-9" />
       <h1 className="text-default-text my-auto text-center text-lg font-bold">
         HANNAH&apos;S LIST LAB
       </h1>
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,3 +1,4 @@
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
@@ -5,7 +6,9 @@ import './index.css';
 import App from './App.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
 );
